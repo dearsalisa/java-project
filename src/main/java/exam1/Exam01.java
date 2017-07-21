01f5067ba88057eddb8a05fbad16ecd69da70f24
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-21 15:20:32 +0700

    [case] A win 1 time -- return "Fifteen-Love"

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -2,17 +2,21 @@ package exam1;
 
 public class Exam01 {
 
+    private int countA = 0;
+
     public Exam01(String firstPerson, String secondPerson) {
     }
 
 
     public void playerAWin() {
+        countA++;
     }
 
     public void playerBWin() {
     }
 
     public String getScore() {
+        if(countA == 1) return "Fifteen-Love";
         return "Love-All";
     }
 
