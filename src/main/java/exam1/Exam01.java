ab6ec9ac7d8bcd4398639ca3234549011658b069
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-21 16:18:20 +0700

    [Exam01] Done

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -17,32 +17,29 @@ public class Exam01 {
         countB++;
     }
 
+    public String convertScoreToString(int score) {
+        if(score == 0) return "Love";
+        else if(score == 1) return "Fifteen";
+        else if(score == 2) return "Thirty";
+        else if(score == 3) return "Forty";
+        else return "";
+    }
+
     public String getScore() {
 
-        String scoreA = "Love";
-        String scoreB = "Love";
+        String scoreA = convertScoreToString(countA);
+        String scoreB = convertScoreToString(countB);
 
         if(countA - countB == 1 && countA >= 4) return "Advantage Player A";
-        else if(countB - countA == 1 && countB >= 4) return "Advantage Player B";
+        if(countB - countA == 1 && countB >= 4) return "Advantage Player B";
 
-        if(countA - countB == 2 && countA >= 4) return "Win for Player A";
-        else if(countB - countA == 2 && countB >= 4) return "Win for Player B";
+        if(countA - countB >= 2 && countA >= 4) return "Win for Player A";
+        if(countB - countA >= 2 && countB >= 4) return "Win for Player B";
 
-        if(countA == countB && countA == 0) return "Love-All";
-        else if(countA == countB && countA == 1) return "Fifteen-All";
-        else if(countA == countB && countA == 2) return "Thirty-All";
-        else if(countA == countB && countA >= 3) return "Deuce";
-
-        if(countA == 1) scoreA = "Fifteen";
-        else if(countA == 2) scoreA = "Thirty";
-        else if(countA == 3) scoreA = "Forty";
-        else if(countA == 4) return "Win for Player A";
-
-        if(countB == 1) scoreB = "Fifteen";
-        else if(countB == 2) scoreB = "Thirty";
-        else if(countB == 3) scoreB = "Forty";
-        else if(countB == 4) return "Win for Player B";
-
-        return scoreA+"-"+scoreB;
+        if(countA == countB) {
+            if(countA >= 3) return "Deuce";
+            else return scoreA+"-All";
+        }
+        else return scoreA+"-"+scoreB;
     }
 }
