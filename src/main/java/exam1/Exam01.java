87891c817a54e11ee5b67075392f482f5973ef06
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-25 14:29:39 +0700

    refactor code : follow business flow

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -4,10 +4,13 @@ public class Exam01 {
 
     private int countA = 0;
     private int countB = 0;
+    private String playerA = "";
+    private String playerB = "";
 
     public Exam01(String firstPerson, String secondPerson) {
+        this.playerA = firstPerson;
+        this.playerB = secondPerson;
     }
-
 
     public void playerAWin() {
         countA++;
@@ -17,33 +20,52 @@ public class Exam01 {
         countB++;
     }
 
-    public String convertScoreToString(int score) {
-        if(score == 0) return "Love";
-        if(score == 1) return "Fifteen";
-        if(score == 2) return "Thirty";
-        if(score == 3) return "Forty";
-        return "";
+    public boolean isEqualScore() {
+        return (countA == countB && countA <= 2);
+    }
+
+    public boolean isNormalScore() {
+        return (countA != countB && countA <= 3 && countB <= 3);
+    }
+
+    public boolean isDeuce() {
+        return (countA == countB && countA >= 3);
+    }
+
+    public boolean isAdvantage() {
+        return (countA >= 3 && countB >= 3 && Math.abs(countA - countB) == 1);
+    }
+
+    public boolean isWin() {
+        return (Math.abs(countA - countB) >= 2);
     }
 
     public String getScore() {
 
-        String scoreA = convertScoreToString(countA);
-        String scoreB = convertScoreToString(countB);
+        String[] score = {"Love", "Fifteen", "Thirty", "Forty"};
 
-        if(countA > 3) {
-            if(countA - countB == 1) return "Advantage Player A";
-            if(countA - countB >= 2) return "Win for Player A";
-        }
-
-        if(countB > 3) {
-            if(countB - countA == 1) return "Advantage Player B";
-            if(countB - countA >= 2) return "Win for Player B";
-        }
-
-        if(countA == countB) {
-            if(countA >= 3) return "Deuce";
-            else return scoreA+"-All";
-        }
-        else return scoreA+"-"+scoreB;
+        if (isEqualScore()) {
+            return String.format("%s-All", score[countA]);
+        } else if (isNormalScore()) {
+            return String.format("%s-%s", score[countA], score[countB]);
+        } else if (isDeuce()) {
+            return "Deuce";
+        } else if (isAdvantage()) {
+            String advantagePlayer = "";
+            if (countA > countB) {
+                advantagePlayer = playerA;
+            } else {
+                advantagePlayer = playerB;
+            }
+            return String.format("Advantage %s", advantagePlayer);
+        } else if (isWin()) {
+            String winPlayer = "";
+            if (countA > countB) {
+                winPlayer = playerA;
+            } else {
+                winPlayer = playerB;
+            }
+            return String.format("Win for %s", winPlayer);
+        } else return "Invalid Input";
     }
 }
